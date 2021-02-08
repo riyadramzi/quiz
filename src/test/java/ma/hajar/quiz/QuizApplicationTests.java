f7ad5c84c8acfed2d45fd61e5ef1c027f2971ec6
RAMZI.RIYAD <ramzi.riyad@ma.ibm.com>
2021-02-08 18:47:48 +0100

    commit files

---
+++ src/test/java/ma/hajar/quiz/QuizApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.hajar.quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
