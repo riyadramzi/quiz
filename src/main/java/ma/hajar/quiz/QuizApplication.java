ba201969a316bbcaa9ec90736751776417daf6f6
RAMZI.RIYAD <ramzi.riyad@ma.ibm.com>
2022-05-31 03:24:30 +0100

    First Commit branch develop

--- src/main/java/ma/hajar/quiz/QuizApplication.java
+++ src/main/java/ma/hajar/quiz/QuizApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QuizApplication {
+	/**
+	 *
+	 * @param args
+	 */
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuizApplication.class, args);
